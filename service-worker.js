c8041eadcf00ae205bcf9ee3213bf7d0c163fa70
Billy Paulsen <C-William.Paulsen@charter.com>
2019-04-26 16:05:03 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.600abea26e1e56f32f3c637b1cd52bbf.js"
+  "/sift-2.0/precache-manifest.510e254c504a7f8249af0c8d1b149ac6.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "sift-2"});
